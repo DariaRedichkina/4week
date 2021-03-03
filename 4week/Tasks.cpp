7945cc6dfb9acfb251f58e8b4c6bb3a4c0061731
Daria <redichkina.ds@phystech.edu>
2021-03-03 22:50:30 +0300

    сделано 2 первых пункта:  - создана последовательность (вектор P1)  - добавлены 5 чисел из cin

--- 4week/Tasks.cpp
+++ 4week/Tasks.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main() {
-	std::cout << "Hello world!";
+	vector <int> P1;
+	for (int i = 0; i < 10; i++) {
+		P1[i] == i+1;
+	}
+	cout << "Enter 5 random numbers\n";
+	for (int j = 0; j < 5; j++) {
+		cin >> P1[j+9];
+	}
+
+
 	return 0;
 }
